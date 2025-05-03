1c7d5bf6313e297c500525e3d7f3baea00bd3339
pedroedu06 <pedro.nascimento182501@gmail.com>
2025-05-03 15:52:00 -0300

    Mudancas no packages

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const path = require("path");
 const fs = require("fs")
 
 const app = express();
+const port = process.env.PORT || 3000;  
+
 app.use(cors());
 app.use(express.static("interface"));
 app.use(cors({origin: "*"}));
